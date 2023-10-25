a138aed020c461aee177e8813538c437bdccdb7c
Leandro F. L <lfluvisotto@gmail.com>
2023-10-25 15:06:20 -0300

    PlugAPILib

--- Models/CreateFraudAnalysisCustomerRequest.php
+++ Models/CreateFraudAnalysisCustomerRequest.php
@@ -7,6 +7,12 @@ use JsonSerializable;
 
 class CreateFraudAnalysisCustomerRequest implements JsonSerializable
 {
+    public string $name;
+    public string $email;
+    public string $phone;
+    public string $identityType;
+    public string $identity;
+    public string $registrationDate;
     /**
      * @var CreateFraudAnalysisCustomerBrowserRequest[]
      */
@@ -15,6 +21,12 @@ class CreateFraudAnalysisCustomerRequest implements JsonSerializable
     public function jsonSerialize(): mixed
     {
         $json = array();
+        $json['name'] = $this->name;
+        $json['email'] = $this->email;
+        $json['phone'] = $this->phone;
+        $json['identityType'] = $this->identityType;
+        $json['identity'] = $this->identity;
+        $json['registrationDate'] = $this->registrationDate;
         $json['browser'] = $this->browser;
 
         return $json;
